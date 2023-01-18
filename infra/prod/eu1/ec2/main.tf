724beeadc5ed07c1838ce4bda15e952c0a4c49b4
Alistair Scott <ali.scott@gmail.com>
2023-01-18 13:41:35 +0000

    test: add monorepo (#29)

---
+++ infra/prod/eu1/ec2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region                      = "eu-west-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_instance" "web_app" {
+  ami           = "ami-674cbc1e"
+  instance_type = "t3.xlarge"
+}
---
+++ infra/prod/us2/rds/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region                      = "us-east-2"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_db_instance" "mysql" {
+  engine         = "mysql"
+  instance_class = "db.t3.xlarge"
+}
